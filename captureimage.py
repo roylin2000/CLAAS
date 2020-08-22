4fa2c6aff6a2b90fd42143708f8b362d6f596628
Celina <celina.shen06@gmail.com>
2020-08-22 16:12:45 -0400

    Created POST requests, can store multiple form data

--- captureimage.py
+++ captureimage.py
@@ -1,54 +1,5 @@
-
-# import the necessary packages
 import cv2
-
-from flask import Flask, render_template, Response, request
-from vidstreaming import VideoCamera
-
 from time import sleep
-
-app = Flask(__name__)
-
-photos = []
-
-@app.route('/')
-def index():
-    # rendering webpage
-    return render_template('index.html')
-def gen(camera):
-    while True:
-        #get camera frame
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/graph', methods = ["POST", "GET"])
-def graph():
-    global photos
-    if request.method == "POST":
-        # file = request.files["file"]
-        # run processing script on file, append data to photos
-
-        
-        photos.append(request.form["name"]) 
-        return "", 204
-    photosList = photos
-    photos = []
-
-
-    
-    return render_template("graph.html", photos = photosList)
-
-if __name__ == '__main__':
-    # defining server ip address and port
-    app.run(host='0.0.0.0',port='5000', debug=True)
-
-"""
 key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
 sleep(2)
@@ -87,4 +38,4 @@ while True:
         print("Camera off.")
         print("Program ended.")
         cv2.destroyAllWindows()
-        break"""
+        break
